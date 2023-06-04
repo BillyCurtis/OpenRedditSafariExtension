8f34e6777c9b85476ca23b647ec5bfd884dcf52a
Billy <38408756+BillyCurtis@users.noreply.github.com>
2023-06-04 04:46:52 +0100

    Add files via upload

---
+++ OpenRedditSafariExtension.appex/content.js
@@ -0,0 +1,24 @@
+browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+    console.log("Received response: ", response);
+});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Received request: ", request);
+});
+
+function afterNavigate() {
+    var locationArr = window.location.pathname.split("/")
+    if (locationArr.includes("r") || locationArr.includes("user")) {
+        window.location.href = `reddit://${window.location.pathname}`
+    }
+}
+
+(document.body || document.documentElement).addEventListener('transitionend',
+  function(/*TransitionEvent*/ event) {
+    if (event.propertyName === 'width' && event.target.id === 'progress') {
+        afterNavigate();
+    }
+}, true);
+// After page load
+afterNavigate();
+
